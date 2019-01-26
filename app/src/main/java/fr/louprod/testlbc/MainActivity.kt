d560b784267c2ccfc2c9667670eaabd0033d8e0b
Mario Robert <mariolourobert@gmail.com>
2019-01-26 14:18:28 +0100

    [TLBC-3] Add BackendModule / implement Retrofit-OkHttp-RxJava / Create clients and Api Service / Create TrackRepository

--- app/src/main/java/fr/louprod/testlbc/MainActivity.kt
+++ app/src/main/java/fr/louprod/testlbc/MainActivity.kt
@@ -2,11 +2,35 @@ package fr.louprod.testlbc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import fr.louprod.backendmodule.models.TrackModel
+import fr.louprod.backendmodule.repositories.TrackRepository
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // For testing :
+        TrackRepository.getAllTracks(
+            object : Observer<List<TrackModel>> {
+                override fun onComplete() {}
+
+                override fun onSubscribe(d: Disposable) {}
+
+                override fun onNext(t: List<TrackModel>) {
+                    Log.d("response", "${t.size}")
+                }
+
+                override fun onError(e: Throwable) {
+                    Log.d("response", "ERROR")
+                    e.printStackTrace()
+                }
+
+            }
+        )
     }
 }
---
+++ backendmodule/src/main/java/fr/louprod/backendmodule/network/RetrofitClients.kt
@@ -0,0 +1,33 @@
+package fr.louprod.backendmodule.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClients {
+    private val lbcStaticUrl = "https://static.leboncoin.fr/"
+
+    val lbcStaticClient: LbcStaticService by lazy {
+        buildApiClient(lbcStaticUrl).create(LbcStaticService::class.java)
+    }
+
+    private fun buildApiClient(apiUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .client(getOkHttpClient())
+            .baseUrl(apiUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+}
