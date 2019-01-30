01f96b9b6cbfbf3a3d78cba8b5eb719725a2413a
Mario Robert <mariolourobert@gmail.com>
2019-01-30 01:21:44 +0100

    [TLBC-22] WIP : appBar Ok but back button doesn't work

--- app/src/main/java/fr/louprod/testlbc/MainActivity.kt
+++ app/src/main/java/fr/louprod/testlbc/MainActivity.kt
@@ -1,13 +1,25 @@
 package fr.louprod.testlbc
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import fr.louprod.testlbc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
+
+    private val navController by lazy {
+        findNavController(R.id.navHost)
+    }
+
+    private val appBarConfig by lazy {
+        AppBarConfiguration.Builder(navController.graph).build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +27,21 @@ class MainActivity : AppCompatActivity() {
             this,
             R.layout.activity_main
         )
+
+
+        binding?.toolbar?.let {
+            NavigationUI.setupWithNavController(
+                it,
+                navController,
+                appBarConfig
+            )
+        }
+
+        setSupportActionBar(binding?.toolbar)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d("click", "11111")
+        return NavigationUI.navigateUp(navController, appBarConfig) || super.onSupportNavigateUp()
     }
 }
